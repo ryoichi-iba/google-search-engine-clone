21d1e2c3cdb576bb89a782ab6daea1c03be64771
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-04-15 21:30:51 +0900

    fix image

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
   <div class="wrapper indexPage">
     <div class="mainSection">
       <div class="logoContainer">
-        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        <img src="assets/images/doodle.png" alt="">
       </div>
 
       <div class="searchContainer">
         <form action="search.php" method="GET">
-          <input type="text" class="searchBox" name="term" placeholder="Enter a search term....">
-          <input type="submit" class="searchBox" value="Search">
+          <input type="text" class="searchBox" name="term">
+          <input type="submit" class="searchButton" value="Search">
         </form>
       </div>
     </div>
