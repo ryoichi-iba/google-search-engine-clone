c5fac7616670f3e1d9937569da0f38d2d460a4a0
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-04-15 21:58:50 +0900

    fix style

--- search.php
+++ search.php
@@ -1,4 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo $_GET["term"];
-?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Welcome to Doodle</title>
+</head>
+
+<body>
+
+  <div class="wrapper">
+    <div class="header">
+      <div class="headerContent">
+        <div class="logoContainer">
+          <a href="index.php">
+            <img src="assets/images/doodle.png" alt="">
+          </a>
+        </div>
+
+        <div class="searchContainer">
+          <form action="search.php" method="GET">
+
+            <div class="searchBarContainer">
+              <input type="text" class="searchBox" name="term">
+              <button> Search</button>
+            </div>
+
+
+          </form>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</body>
+
+</html>
