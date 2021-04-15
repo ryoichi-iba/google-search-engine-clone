b14a9c50d96ed78494edb37a747b52c42cb3bce1
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-04-15 20:53:34 +0900

    add base files

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Welcome to Doodle</title>
+</head>
+
+<body>
+
+  <div class="wrapper indexPage">
+    <div class="mainSection">
+      <div class="logoContainer">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+      </div>
+
+      <div class="searchContainer">
+        <form action="search.php" method="GET">
+          <input type="text" class="searchBox" name="term" placeholder="Enter a search term....">
+          <input type="submit" class="searchBox" value="Search">
+        </form>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
